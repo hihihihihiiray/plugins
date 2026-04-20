55c2cfca7de5b298df91f2685a679d850b5e58e5
raymondnoodles <raymondvu879@gmail.com>
2026-04-20 14:59:08 -0500

    Update videasy.js

--- providers/videasy.js
+++ providers/videasy.js
@@ -11,8 +11,8 @@ const DECRYPT_API = 'https://enc-dec.app/api/dec-videasy';
 const HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
     'Accept': '*/*',
-    'Origin': 'https://fmovies.gd',
-    'Referer': 'https://fmovies.gd/'
+    'Origin': 'https://cineby.sc',
+    'Referer': 'https://cineby.sc/'
 };
 
 // Server configurations
