1001ce47bdfbc2b35de5a6ce4e32a14e0a7b761a
raymondnoodles <raymondvu879@gmail.com>
2026-04-26 20:37:08 -0500

    Update dahmermovies.js

--- providers/dahmermovies.js
+++ providers/dahmermovies.js
@@ -6,7 +6,7 @@ console.log('[DahmerMovies] Initializing Dahmer Movies scraper');
 // Constants
 const TMDB_API_KEY = "1c29a5198ee1854bd5eb45dbe8d17d92";
 const DAHMER_MOVIES_API = 'https://a.111477.xyz';
-const TIMEOUT = 5000; // 5 seconds
+const TIMEOUT = 15000; // 15 seconds
 
 const BATCH_SIZE = 3;          // links resolved in parallel per batch
 const BATCH_GAP_MS = 1200;      // gap between batches (only paid when a 429 occurred)
