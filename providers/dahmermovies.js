b592d22ca2aca8666a558f5c155c6aef1ceab6be
raymondnoodles <raymondvu879@gmail.com>
2026-04-27 17:48:18 -0500

    Update dahmermovies.js

--- providers/dahmermovies.js
+++ providers/dahmermovies.js
@@ -34,6 +34,9 @@ function makeRequest(url, options = {}) {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
             ...options.headers
         },
         ...options
